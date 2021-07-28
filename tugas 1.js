e728c615807584ae3b7586f7a61603b8fec039f5
muklisxtc24 <87222847+muklisxtc24@users.noreply.github.com>
2021-07-28 21:14:20 +0700

    Add files via upload

---
+++ tugas 1.js
@@ -0,0 +1,77 @@
+// tugas object 1
+var kendaraan = {
+  nama: "CBR150",
+  merek: "HONDA",
+  wana: "putih",
+  gas: function () {
+    return "motor jalan";
+  },
+  rem: function () {
+    return "motor berhenti";
+  },
+};
+console.log(kendaraan);
+
+
+// tugas object 2
+var biodatadiri = {
+  nama: "mukhlis Abdul Aziz",
+  "asal daerah": "bekasi",
+  "jenis kelamin": "laki-laki",
+  berkata: function () {
+    console.log("hello guys nama saya mukhlis");
+  },
+};
+console.log(biodatadiri);
+console.log(biodatadiri.berkata());
+
+
+//tugas object 3
+var buah = {
+  nama: "semangka",
+  rasa: "manis",
+  wana: "merah",
+  dimakan: function () {
+    return "dikupas dulu";
+  },
+  dibuang: function () {
+    return "buang aaja ";
+  },
+};
+console.log(buah);
+
+
+//tugas object 4
+var indonesia = {
+  ibukota: "jakarta",
+  matauang: "rupiah",
+  jumlahpenduduk: "270,000,000",
+  rakyat: function () {
+    return "menjalankan urusan rakyat";
+  },
+  penjabat: function () {
+    return "menjalankan tugas pennjabat";
+  },
+};
+console.log(indonesia);
+
+//tugas object 5
+var hp = {
+  nama: "xiaomi 10s",
+  merk: "xiaomi",
+  warna: "putih",
+  dinyalakan: function () {
+    return "pencet tombol power";
+  },
+  kegunaan: function () {
+    return "untuk mempermudah perkerjaan manusia";
+  },
+};
+console.log(buah);
+
+  
+
+
+
+
+
